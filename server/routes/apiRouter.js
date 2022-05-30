f93a1210da474e803517e0af3d98b7c46b2f2caf
= <=>
2022-05-30 16:35:27 -0400

    updated RSS queries. Used class syntax to create source obj for adding new soources. Cleaned up RSS feed queries and use of promise.all, add some parameters for the query strings, hopefully we can use those instead of requst body. Also added more extensive error handling and commenting throughout.

--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -5,25 +5,27 @@ const Router = express.Router();
 
 
 
-Router.get('/',
-  apiController.getTransformRSS,
-  (req, res) => {
-    return res.status(200).json({ data: res.locals.data })
-  })
 
 
 Router.post('/login',
-  // pgContorller.validateUser,
-  (req, res) => {
-    console.log(req.session);
-    return res.status(200).json(res.locals.validationResponse);
-  })
+pgContorller.validateUser,
+(req, res) => {
+  console.log(req.session);
+  return res.status(200).json(res.locals.validationResponse);
+})
 
 
 Router.get('/user')
 Router.post('/user')
 Router.put('/user')
 
+
+
+Router.get('/',
+  apiController.getTransformRSS,
+  (req, res) => {
+    return res.status(200).json({ data: res.locals.data })
+  })
 
 
 module.exports = Router
